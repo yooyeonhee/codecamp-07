b00cf905fba12c8586eee49ca0ab2913d74363a8
yooyeonhee <devyh0629@gmail.com>
2022-05-16 19:28:03 +0900

    day06

--- programmers/06.js
+++ programmers/06.js
@@ -7,3 +7,54 @@
     
 //     return answer;
 // }
+
+
+// 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다. 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다. 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+// arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+// arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+// 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+// function solution(arr)
+// {
+//     var answer = [];
+//     answer.push(arr[0])
+//     for(let i=0; i<arr.length-1; i++){
+//         if (arr[i]!==arr[i+1]){
+//             answer.push(arr[i+1])
+//         }
+//     }
+//     return answer;
+// }
+
+
+// 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+// 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+
+// function solution(phone_number) {
+//     var answer = '';
+//     let arr = phone_number.split("")
+//     for(i=0; i<phone_number.length-4; i++){
+//       arr[i] = '*'
+//     }
+//     answer = arr.join("")
+//     console.log(answer)
+//     return answer;
+// }
+
+// for(let i =0; i<phone_number.length; i++){
+//     if(i < phone_number.length -4){
+//         answer += "*";
+//     }
+//     else{
+//         answer += phone_number[i];
+//     }
+// }
+// return answer;
+
+
+//padStart Slice subString 자주 사용
+
+// function solution(phone_number) {
+// answer = answer.padStart(phone_number.length -4, "*")
+// answer += phone_number.substring(phone_number.length-4);
+// return answer;
