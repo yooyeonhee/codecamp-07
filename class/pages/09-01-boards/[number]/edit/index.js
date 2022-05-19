78c350e590e8ff23607824c049a00abede3c7cd3
yooyeonhee <devyh0629@gmail.com>
2022-05-19 15:50:23 +0900

    detail default

---
+++ class/pages/09-01-boards/[number]/edit/index.js
@@ -0,0 +1,29 @@
+import BoardWrite from "../../../../src/components/units/board/09-write/BoardWrite.container";
+import { gql, useQuery} from '@apollo/client'
+import { useRouter } from 'next/router'
+
+const FETCH_BOARD = gql 
+`
+    query fetchBoard($number: Int){
+        fetchBoard(number: $number){
+            number
+            writer
+            title
+            contents
+        }
+    }
+`
+
+export default function BoardEditPage(){
+
+const router = useRouter()
+
+const {data} = useQuery(FETCH_BOARD, {
+        variables: {number: Number(router.query.number)}
+    })
+
+
+    return(
+        <BoardWrite isEdit={true} boardData={data}/>
+    )
+}
---
+++ class/src/components/units/board/09-write/BoardWrite.presenter.js
@@ -0,0 +1,17 @@
+import * as S from "./BoardWrite.styles"
+export default function BoardWriteUI(props){
+
+
+    return (
+        <div>
+            <h1>{props.isEdit ? "수정" : "등록"}페이지</h1>
+            작성자 : <S.WriterInput type="text" onChange={props.onChangeName} defaultValue={props.boardData?.fetchBoard.writer}/>
+            제목 : <input type="text" onChange={props.onChangeTitle} defaultValue={props.boardData?.fetchBoard.title}/>
+            내용 : <input type="text" onChange={props.onChangeContents} defaultValue={props.boardData?.fetchBoard.contents}/>
+            <div>{props.data?.message}</div>
+            <button onClick={props.isEdit ? props.onClickUpdate : props.onClickGraphqlApi}>
+                {props.isEdit ? "수정": "등록"}하기!
+            </button>
+        </div>
+    )
+}
