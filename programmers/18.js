f4647bf60444c05e249ede3f0099d6f6363a82e5
yooyeonhee <devyh0629@gmail.com>
2022-06-02 18:19:23 +0900

    day 17

---
+++ programmers/18.js
@@ -0,0 +1,13 @@
+// 길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+
+// 이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+
+// function solution(a, b) {
+//     var answer = 0;
+//     for(let i=0; i<a.length; i++){
+//         answer += (a[i]*b[i])
+//     }
+//     return answer;
+// }
+
+// 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
